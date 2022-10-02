d958d680ae37f23ba150ee23381e8967e5e970a2
AleksaOpacic <aleksaopacic988@gmail.com>
2022-10-02 12:46:41 +0200

    Fix lint

--- tracker/block_results.go
+++ tracker/block_results.go
@@ -68,6 +68,7 @@ func (b blockTrackerResults) getTestFileName(from uint64, to uint64) string {
 	if len(fileName) != 2 {
 		return b.outputFilePath
 	}
+
 	fromString := strconv.FormatUint(from, 10)
 	toString := strconv.FormatUint(to, 10)
 
